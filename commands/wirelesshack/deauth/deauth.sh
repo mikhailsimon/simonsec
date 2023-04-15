30b70057bfa00bc3d5e93efb655e1dc715890d21
mikhailsimon <mikhailsimon0705@gmail.com>
2023-04-15 21:17:19 +0800

    fixed typo

--- commands/wirelesshack/deauth/deauth.sh
+++ commands/wirelesshack/deauth/deauth.sh
@@ -4,10 +4,10 @@ check_adapter() {
 iwconfig | zenity --text-info --title="Available Adapter List" --width=500 --height=300
 }
 
-if [ "$prompt1" == "Deauth Attack" ]
+if [ "$prompt1" == "Injection Test" ]
 then
  check_adapter
- wlan=$(zenity --entry --title="Card Interface" --text="Interface:" --width=300)
+ wlan=$(ip -o link show | awk -F': ' '{print $2}' | zenity --list --title="Network Interfaces" --column="Interface Name")
   if [ -z "$wlan" ]
    then
      zenity --info --title="Error" --text="Please insert input"
